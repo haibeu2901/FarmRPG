37d18b7297ba4706252457c2ce9891dc15432c67
haibeu2901 <beu2901@gmail.com>
2025-05-05 13:15:14 +0700

    run game in main

--- FarmRPG/Program.cs
+++ FarmRPG/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace FarmRPG
+﻿using FarmRPG.GameLogic;
+
+namespace FarmRPG
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Enter your farmer name: ");
+            string farmerName = Console.ReadLine(); 
+            Game game = new Game(farmerName);
+            game.Run();
         }
     }
 }
