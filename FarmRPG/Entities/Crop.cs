336edaba99b84e0c14adcdafb70620c4240cf5e2
haibeu2901 <beu2901@gmail.com>
2025-05-05 12:48:14 +0700

    make changes to crop class

--- FarmRPG/Entities/Crop.cs
+++ FarmRPG/Entities/Crop.cs
@@ -8,6 +8,36 @@ namespace FarmRPG.Entities
 {
     public class Crop
     {
-        public 
+        public string Type { get; private set; }
+        public int GrowthStage { get; private set; } // 0: Seeded, 1: Growing, 2: Mature
+
+        public Crop(string type)
+        {
+            Type = type;
+            GrowthStage = 0;
+        }
+
+        public void grow()
+        {
+            if (GrowthStage < 2)
+            {
+                GrowthStage++;
+                if (GrowthStage == 2)
+                {
+                    Console.WriteLine($"{Type} is now mature and ready to harvest!");
+                }
+            }
+        }
+
+        public bool isMature()
+        {
+            return GrowthStage == 2;
+        }
+
+        public void harvest()
+        {
+            Console.WriteLine($"Harvested {Type}");
+            GrowthStage = 0; // Reset growth stage after harvesting
+        }
     }
 }
