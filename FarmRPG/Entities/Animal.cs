297272b2457b032d188ffea6de58ba7aff93cccb
haibeu2901 <beu2901@gmail.com>
2025-05-05 11:55:43 +0700

    create animal class

--- FarmRPG/Entities/Animal.cs
+++ FarmRPG/Entities/Animal.cs
@@ -8,10 +8,30 @@ namespace FarmRPG.Entities
 {
     public class Animal
     {
-        public String Name { get; set; }
-        public String Type { get; set; }
-        public int Hunger { get; set; }
+        public String Name { get; private set; }
+        public String Type { get; private set; }
+        public int Hunger { get; private set; }
 
-        
+        public Animal(string name, string type)
+        {
+            Name = name;
+            Type = type;
+            Hunger = 5; // Initial hunger level
+        }
+
+        public void feed()
+        {
+            Console.WriteLine($"{Name} the {Type} has been fed.");
+            Hunger = 0;
+        }
+
+        public void update()
+        {
+            Hunger++;
+            if (Hunger > 10)
+            {
+                Console.WriteLine($"{Name} the {Type} is very hungry");
+            }
+        }
     }
 }
